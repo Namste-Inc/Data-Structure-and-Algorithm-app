7e2bf42adabfbc5e37c0360c971ad1a868c84c1b
ddunig2 <36016544+ddunig2@users.noreply.github.com>
2019-08-14 02:30:57 -0400

    Added pop up dialogs

---
+++ app/src/main/java/com/example/lugdu/datastructuresandalgorithms/FragmentDialog.java
@@ -0,0 +1,37 @@
+package com.example.lugdu.datastructuresandalgorithms;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+public class FragmentDialog extends AppCompatDialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        String text = getArguments().getString("text");
+        int color = getArguments().getInt("color");
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_dialog, null);
+        View view2 = (Hexagon)view.findViewById(R.id.hexagon);
+        View vie = (TextView)view.findViewById(R.id.textView2);
+        ((Hexagon) view2).setHexiText(text);
+        ((Hexagon) view2).setHexiColor(color);
+        GradientDrawable drawable = new GradientDrawable();
+        drawable.setCornerRadius(90);
+        int[] colors = {Color.BLACK,color};
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            drawable.setColors(colors);
+            vie.setBackground(drawable);
+        }
+        view.setBackgroundColor(Color.TRANSPARENT);
+        builder.setView(view);
+        return builder.create();
+    }
+}
