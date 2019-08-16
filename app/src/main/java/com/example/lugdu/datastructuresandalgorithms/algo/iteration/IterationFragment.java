e43fb9809eb71e579abd95aa108b9c55c49e7263
arashid-stac <allirashid97@gmail.com>
2019-08-16 15:00:02 -0400

    Added Toolbars to the structures and algorithms that only have one tab.

--- app/src/main/java/com/example/lugdu/datastructuresandalgorithms/algo/iteration/IterationFragment.java
+++ app/src/main/java/com/example/lugdu/datastructuresandalgorithms/algo/iteration/IterationFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.lugdu.datastructuresandalgorithms.R;
 
@@ -15,7 +17,11 @@ public class IterationFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_iteration, container,false);
+        view = inflater.inflate(R.layout.middle_fragment, container,false);
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        toolbar.setBackgroundColor(getArguments().getInt("color"));
+        TextView textView = view.findViewById(R.id.textviewtoolbar);
+        textView.setText("Iteration");
         return view;
     }
 }
--- app/src/main/java/com/example/lugdu/datastructuresandalgorithms/dataS/Stacks/StacksFragment.java
+++ app/src/main/java/com/example/lugdu/datastructuresandalgorithms/dataS/Stacks/StacksFragment.java
@@ -3,9 +3,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.lugdu.datastructuresandalgorithms.R;
 
@@ -14,7 +16,11 @@ public class StacksFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_stacks, container,false);
+        view = inflater.inflate(R.layout.middle_fragment, container,false);
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        toolbar.setBackgroundColor(getArguments().getInt("color"));
+        TextView textView = view.findViewById(R.id.textviewtoolbar);
+        textView.setText("Stacks");
         return view;
     }
 }
