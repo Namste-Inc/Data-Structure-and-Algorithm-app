f516ff4e4e8d7524abc1d6669924a20403ca5c4e
arashid-stac <allirashid97@gmail.com>
2019-08-18 13:46:21 -0400

    Changes
    1) Added a tab for the Iteration Hexagon

---
+++ app/src/main/java/com/example/lugdu/datastructuresandalgorithms/algo/iteration/DoWhileLoopFragment.java
@@ -0,0 +1,19 @@
+package com.example.lugdu.datastructuresandalgorithms.algo.iteration;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lugdu.datastructuresandalgorithms.R;
+
+public class DoWhileLoopFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_do_while_loop, container,false);
+    }
+}
--- app/src/main/java/com/example/lugdu/datastructuresandalgorithms/algo/iteration/IterationFragment.java
+++ app/src/main/java/com/example/lugdu/datastructuresandalgorithms/algo/iteration/IterationFragment.java
@@ -1,19 +1,25 @@
 package com.example.lugdu.datastructuresandalgorithms.algo.iteration;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.lugdu.datastructuresandalgorithms.PagerAdapter;
 import com.example.lugdu.datastructuresandalgorithms.R;
 import com.example.lugdu.datastructuresandalgorithms.algo.AlgorithmFragment;
+
+import java.util.HashMap;
 
 public class IterationFragment extends Fragment {
     View view;
@@ -23,6 +29,9 @@ public class IterationFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.middle_fragment, container,false);
         getActivity().getWindow().setStatusBarColor(getArguments().getInt("color"));
+        TabLayout tabLayout = view.findViewById(R.id.tabview);
+        tabLayout.setBackgroundColor(getArguments().getInt("color"));
+        tabLayout.setSelectedTabIndicatorColor(Color.WHITE);
         Toolbar toolbar = view.findViewById(R.id.toolbar);
         toolbar.setBackgroundColor(getArguments().getInt("color"));
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -33,6 +42,40 @@ public class IterationFragment extends Fragment {
         });
         TextView textView = view.findViewById(R.id.textviewtoolbar);
         textView.setText("Iteration");
+
+        final ViewPager viewPager = view.findViewById(R.id.pager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("For Loop"));
+        tabLayout.addTab(tabLayout.newTab().setText("While Loop"));
+        tabLayout.addTab(tabLayout.newTab().setText("Do-While Loop"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        HashMap<Integer, Fragment> iteration = new HashMap<>();
+        iteration.put(0,new ForLoopFragment());
+        iteration.put(1,new WhileLoopFragment());
+        iteration.put(2,new DoWhileLoopFragment());
+
+        PagerAdapter pagerAdapter = new PagerAdapter(getFragmentManager(),tabLayout.getTabCount(),iteration);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
         return view;
     }
 }
