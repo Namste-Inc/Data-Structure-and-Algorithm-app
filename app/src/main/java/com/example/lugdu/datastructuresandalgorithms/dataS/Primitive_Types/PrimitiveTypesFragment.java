0c4ed97cc773a99d55b02c3cb30cbcf78f4966fb
arashid-stac <allirashid97@gmail.com>
2019-08-18 15:20:28 -0400

    Changes
    1) Added primitive types fragments to layout.
    2) Created Primitive Types package and added it to dataS.
    3) Created the necessary Java files in the Primitive_Types package.

---
+++ app/src/main/java/com/example/lugdu/datastructuresandalgorithms/dataS/Primitive_Types/PrimitiveTypesFragment.java
@@ -0,0 +1,84 @@
+package com.example.lugdu.datastructuresandalgorithms.dataS.Primitive_Types;
+
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.lugdu.datastructuresandalgorithms.PagerAdapter;
+import com.example.lugdu.datastructuresandalgorithms.R;
+import com.example.lugdu.datastructuresandalgorithms.dataS.DataStructureFragment;
+
+import java.util.HashMap;
+
+public class PrimitiveTypesFragment extends Fragment {
+    View view;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.middle_fragment, container,false);
+        getActivity().getWindow().setStatusBarColor(getArguments().getInt("color"));
+        TabLayout tabLayout = view.findViewById(R.id.tabview);
+        tabLayout.setBackgroundColor(getArguments().getInt("color"));
+        tabLayout.setSelectedTabIndicatorColor(Color.WHITE);
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        toolbar.setBackgroundColor(getArguments().getInt("color"));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DataStructureFragment()).commit();
+            }
+        });
+        TextView textView = view.findViewById(R.id.textviewtoolbar);
+        textView.setText("Primitive Types");
+        final ViewPager viewPager = view.findViewById(R.id.pager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Integer"));
+        tabLayout.addTab(tabLayout.newTab().setText("String"));
+        tabLayout.addTab(tabLayout.newTab().setText("Float"));
+        tabLayout.addTab(tabLayout.newTab().setText("Boolean"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+
+        HashMap<Integer, Fragment> primitiveTypes = new HashMap<>();
+        primitiveTypes.put(0,new IntegerFragment());
+        primitiveTypes.put(1,new StringFragment());
+        primitiveTypes.put(2,new FloatFragment());
+        primitiveTypes.put(3,new BooleanFragment());
+
+        PagerAdapter pagerAdapter = new PagerAdapter(getFragmentManager(),tabLayout.getTabCount(),primitiveTypes);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        return view;
+    }
+}
