a9a6084b89bb429a4d27aad66686dae67c743d9b
arashid-stac <allirashid97@gmail.com>
2019-09-03 21:59:55 -0400

    Changes
    Began implementing insertion sort algorithm

--- app/src/main/java/com/example/lugdu/datastructuresandalgorithms/algo/Sort/InsertionSortFragment.java
+++ app/src/main/java/com/example/lugdu/datastructuresandalgorithms/algo/Sort/InsertionSortFragment.java
@@ -1,19 +1,168 @@
 package com.example.lugdu.datastructuresandalgorithms.algo.Sort;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.lugdu.datastructuresandalgorithms.CircleText;
 import com.example.lugdu.datastructuresandalgorithms.R;
 
+import java.util.Arrays;
+
 public class InsertionSortFragment extends Fragment {
+    View view;
+//    int arr[] = {5,3,4,7,6,10,2,13,8};
+    int arr[];
+    TextView tArr[];
+    TextView explanationText;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_insertion_sort, container, false);
+        view = inflater.inflate(R.layout.fragment_insertion_sort, container,false);
+        final Button button1 = view.findViewById(R.id.button1);
+        final EditText editText = view.findViewById(R.id.topBox);
+        explanationText = view.findViewById(R.id.explanationText);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!editText.isInEditMode()){
+                    String[] afterC = editText.getText().toString().split(": ");
+                    if(afterC.length > 1){
+                        editText.setText(afterC[1]);
+                    }
+                    editText.setSelection(editText.getText().length());
+                }
+            }
+        });
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE){
+                    if(entryGood(editText.getText().toString())) {
+                        if(!editText.getText().toString().equals("")){
+                            parseArray(editText.getText().toString());
+                            initArray();
+                        }
+                        editText.setText(editText.getText());
+                        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+                        editText.clearFocus();
+                        editText.setSelection(editText.getText().length());
+                        return true;
+                    }
+                    else{
+                        Toast.makeText(getContext(),"Invalid Entry", Toast.LENGTH_LONG).show();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+            @Override
+            public void onClick(View v) {
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+
+                        System.out.println("Original array: " + Arrays.toString(arr));
+                        insertionSort(arr);
+                        System.out.println("Sorted array: " + Arrays.toString(arr));
+
+                    }
+                };
+                Thread thread = new Thread(runnable);
+                thread.start();
+            }
+        });
+        return view;
+    }
+
+    public void insertionSort(int[] array) {
+        int arrayLength = array.length;
+
+        for (int j = 1; j < arrayLength; j++) {
+            System.out.println("beginning here: " + Arrays.toString(array));
+
+            int key = array[j];
+            int i = j-1;
+
+            //while i is greater than negative 1 and while the element at position i is greater than what's in front of it
+            while ((i> -1) && (array[i] > key)) {
+                //make the element in front of i equal to i
+
+
+                //NOTE: instead of overwriting, maybe do a swap
+                //array[i+1] = array[i];
+                swapArrInt(i+1, i);
+                i--;
+                System.out.println("in the while: " + Arrays.toString(array));
+            }
+            //make the first element in the array equal to the current key
+            array[i+1] = key;
+        }
+    }
+    public void swapArrInt(int pos1, int pos2){
+        int pos = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = pos;
+    }
+
+    public boolean entryGood(String entry){
+        if(entry.startsWith(",") || entry.endsWith(",") || entry.contains(",,")){
+            return false;
+        }
+        return true;
+    }
+
+    public void parseArray(String arr){
+        String[] arr1 = arr.split(",");
+        this.arr = new int[arr1.length];
+        if(arr.equals("")){
+            this.arr = null;
+            return;
+        }
+        for(int i = 0; i<arr1.length; i++){
+            this.arr[i] = Integer.parseInt(arr1[i]);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    public void initArray(){
+        int len = arr.length;
+        tArr = new TextView[len];
+        RelativeLayout insertPoint = view.findViewById(R.id.lView);
+        if(insertPoint.getChildCount() > 0){
+            insertPoint.removeAllViews();
+        }
+        for(int i = 0; i<len; i++){
+            TextView textView = new CircleText(getContext());
+            textView.setText(arr[i] + "");
+            tArr[i] = textView;
+            textView.setX(i * 130);
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(120,120);
+            layoutParams.setMargins(20,0,0,25);
+            insertPoint.addView(textView, layoutParams);
+        }
     }
 }
