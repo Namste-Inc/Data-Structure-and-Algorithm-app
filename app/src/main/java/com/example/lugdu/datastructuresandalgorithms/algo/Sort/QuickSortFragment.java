bbccd072a4987ccff687ebd4728a70ba5d244772
ddunig2 <36016544+ddunig2@users.noreply.github.com>
2019-09-12 15:09:12 -0400

    Added animation for quick sort.

--- app/src/main/java/com/example/lugdu/datastructuresandalgorithms/algo/Sort/QuickSortFragment.java
+++ app/src/main/java/com/example/lugdu/datastructuresandalgorithms/algo/Sort/QuickSortFragment.java
@@ -1,5 +1,6 @@
 package com.example.lugdu.datastructuresandalgorithms.algo.Sort;
 
+import android.animation.ObjectAnimator;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
@@ -47,44 +48,44 @@ public class QuickSortFragment extends Fragment {
         final EditText editText = view.findViewById(R.id.topBox);
         explanationText = view.findViewById(R.id.explanationText);
         isRunning = false;
-//        editText.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                if(!editText.isInEditMode()){
-//                    String[] afterC = editText.getText().toString().split(": ");
-//                    if(afterC.length > 1){
-//                        editText.setText(afterC[1]);
-//                    }
-//                    editText.setSelection(editText.getText().length());
-//                }
-//            }
-//        });
-//        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-//            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
-//            @Override
-//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-//                if(actionId == EditorInfo.IME_ACTION_DONE){
-//                    if(entryGood(editText.getText().toString())) {
-//                        if(!editText.getText().toString().equals("")){
-//                            parseArray(editText.getText().toString());
-//                            initArray();
-//                        }
-//                        editText.setText(editText.getText());
-//                        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-//                        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
-//                        editText.clearFocus();
-//                        editText.setSelection(editText.getText().length());
-//                        return true;
-//                    }
-//                    else{
-//                        Toast.makeText(getContext(),"Invalid Entry", Toast.LENGTH_LONG).show();
-//                        return true;
-//                    }
-//                }
-//                return false;
-//            }
-//        });
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!editText.isInEditMode()){
+                    String[] afterC = editText.getText().toString().split(": ");
+                    if(afterC.length > 1){
+                        editText.setText(afterC[1]);
+                    }
+                    editText.setSelection(editText.getText().length());
+                }
+            }
+        });
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE){
+                    if(entryGood(editText.getText().toString())) {
+                        if(!editText.getText().toString().equals("")){
+                            parseArray(editText.getText().toString());
+                            initArray();
+                        }
+                        editText.setText(editText.getText());
+                        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+                        editText.clearFocus();
+                        editText.setSelection(editText.getText().length());
+                        return true;
+                    }
+                    else{
+                        Toast.makeText(getContext(),"Invalid Entry", Toast.LENGTH_LONG).show();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
         button1.setOnClickListener(new View.OnClickListener() {
             @TargetApi(Build.VERSION_CODES.LOLLIPOP)
             @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
@@ -93,44 +94,40 @@ public class QuickSortFragment extends Fragment {
                 Runnable runnable = new Runnable() {
                     @Override
                     public void run() {
-//                        if(arr == null || arr.length == 0){
-//                            getActivity().runOnUiThread(new Runnable() {
-//                                @Override
-//                                public void run() {
-//                                    Toast.makeText(getContext(),"Must enter an array", Toast.LENGTH_LONG).show();
-//                                }
-//                            });
-//                        }
-//                        else {
-//                            if(isRunning){
-//                                getActivity().runOnUiThread(new Runnable() {
-//                                    @Override
-//                                    public void run() {
-//                                        Toast.makeText(getContext(),"Animation running, please wait", Toast.LENGTH_LONG).show();
-//                                    }
-//                                });
-//                            }
-//                            else{
-//                                getActivity().runOnUiThread(new Runnable() {
-//                                    @Override
-//                                    public void run() {
-//                                        editText.setEnabled(false);
-//                                    }
-//                                });
-//                                int arr[] = {3, 44, 38, 5, 7};
-//                                quickSort(arr, 3, 7);
-//                                System.out.println(arr);
-//
-//                                getActivity().runOnUiThread(new Runnable() {
-//                                    @Override
-//                                    public void run() {
-//                                        editText.setEnabled(true);
-//                                    }
-//                                });
-//                            }
-//                        }
-                        int arr[] = {3, 6,4,8,1,5};
-                        quickSort(arr, 0, 5);
+                        if(arr == null || arr.length == 0){
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getContext(),"Must enter an array", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+                        else {
+                            if(isRunning){
+                                getActivity().runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(getContext(),"Animation running, please wait", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }
+                            else{
+                                getActivity().runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        editText.setEnabled(false);
+                                    }
+                                });
+                                quickSort(arr, 0, arr.length - 1);
+
+                                getActivity().runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        editText.setEnabled(true);
+                                    }
+                                });
+                            }
+                        }
 
                     }
                 };
@@ -180,38 +177,92 @@ public class QuickSortFragment extends Fragment {
     }
 
     public void quickSort(int arr[], int begin, int end) {
+        final int finalEnd = end;
         if (begin < end) {
             int partitionIndex = partition(arr, begin, end);
 
             quickSort(arr, begin, partitionIndex-1);
             quickSort(arr, partitionIndex+1, end);
         }
-        System.out.println("From here: " + Arrays.toString(arr));
+        else if(begin == end){
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    ((CircleText) tArr[finalEnd]).sorted();
+                }
+            });
+        }
     }
 
     private int partition(int arr[], int begin, int end) {
+        final int finalEnd = end;
         int pivot = arr[end];
         int i = (begin-1);
-        System.out.println("Here's the pivot: " + pivot);
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ((CircleText) tArr[finalEnd]).select(true);
+            }
+        });
 
         for (int j = begin; j < end; j++) {
             if (arr[j] <= pivot) {
                 i++;
 
-                int swapTemp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = swapTemp;
-                System.out.println("In if statement: " + Arrays.toString(arr));
+                swapArrInt(arr, i, j);
+                swapAni(i, j);
+            }
+            pause(Thread.currentThread(), 1000);
+        }
+
+        final int finalI = i + 1;
+        swapArrInt(arr, end, i + 1);
+        swapAni(end , i + 1);
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ((CircleText) tArr[finalI]).sorted();
+            }
+        });
+        return i+1;
+    }
+
+    public void swapAni(int firstPos, int secondPos){
+        final int pos1 = firstPos;
+        final int pos2 = secondPos;
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextView txt1 = tArr[pos1];
+                TextView txt2 = tArr[pos2];
+                float x1 = txt1.getX() - leftMargin;
+                float x2 = txt2.getX() - leftMargin;
+                ObjectAnimator animation = ObjectAnimator.ofFloat(txt1, "translationX", x2);
+                animation.setDuration(1000);
+                ObjectAnimator animation2 = ObjectAnimator.ofFloat(txt2, "translationX", x1);
+                animation2.setDuration(1000);
+                animation.start();
+                animation2.start();
+                TextView pos = tArr[pos1];
+                tArr[pos1] = tArr[pos2];
+                tArr[pos2] = pos;
+            }
+        });
+    }
+
+    public void swapArrInt(int[] arr, int pos1, int pos2){
+        int pos = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = pos;
+    }
+
+    public void pause(Thread thread, int time){
+        synchronized (thread){
+            try {
+                Thread.currentThread().wait(time);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
-        System.out.println("After if: " + Arrays.toString(arr));
-
-        int swapTemp = arr[i+1];
-        arr[i+1] = arr[end];
-        arr[end] = swapTemp;
-
-        System.out.println("After one partition: " + Arrays.toString(arr));
-
-        return i+1;
     }
 }
