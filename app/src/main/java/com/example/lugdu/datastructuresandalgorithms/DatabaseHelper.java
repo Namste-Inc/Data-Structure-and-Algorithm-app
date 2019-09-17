613d1fdc8661f4584575b3ac52effdcc68422c09
ddunig2 <36016544+ddunig2@users.noreply.github.com>
2019-09-17 14:25:10 -0400

    ...

--- app/src/main/java/com/example/lugdu/datastructuresandalgorithms/DatabaseHelper.java
+++ app/src/main/java/com/example/lugdu/datastructuresandalgorithms/DatabaseHelper.java
@@ -1,12 +1,13 @@
 package com.example.lugdu.datastructuresandalgorithms;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DB_NAME = "DSADB.db";
-    public static final String TABLE_NAME = "BubbleSort";
+    public static final String TABLE_NAME = "HeapSort";
     public static final String COL_1 = "Id";
     public static final String COL_2 = "Definition";
     public static final String COL_3 = "Step";
@@ -16,19 +17,24 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(Context context){
         super(context, DB_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
+         SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query  = "create table " + TABLE_NAME + "(" + COL_1 + " INTEGER PRIMARY KEY AUTOINCREMENT," + COL_2 + " TEXT," + COL_3 + " TEXT," + COL_4 + " BLOB)";
-        System.out.println(query);
-        db.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if exists " + TABLE_NAME);
         onCreate(db);
+    }
+    public void insertData(String tableName, String COL_2,String COL_3){
+        //SQLiteDatabase db = this.getWritableDatabase();
+        //ContentValues contentValues = new ContentValues();
+        //contentValues.put(COL_2, );
+    }
+    public void addTable(){
+        //SQLiteDatabase db = this.getWritableDatabase();
+
     }
 }
--- app/src/main/java/com/example/lugdu/datastructuresandalgorithms/MainActivity.java
+++ app/src/main/java/com/example/lugdu/datastructuresandalgorithms/MainActivity.java
@@ -31,6 +31,31 @@ public class MainActivity extends AppCompatActivity  {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
         myDB = new DatabaseHelper(this);
+
+        myDB.insertData("BubbleSort", null, "Iterates Through array and see if succeeding number if less than the current number");
+        myDB.insertData("BubbleSort", null, "If the current number is less than the succeeding number the two numbers swap");
+        myDB.insertData("BubbleSort", null, "After each iteration the next number starting from the back is sorted");
+
+        myDB.insertData("SelectionSort", null, "Starting from the beginning of the array, sets the next item in the array to be the minimum value in the array");
+        myDB.insertData("SelectionSort", null, "Iterates through array to find a lower value then the minimum value and swaps it with the old minimum value making it the new minimum value");
+        myDB.insertData("SelectionSort", null, "Each preceding item in the array will be sorted starting from the front after each iteration");
+
+        myDB.insertData("InsertionSort", null, "Starting with the second item in the array for as long as the number to the left is less than the current number swap current number with the number on its left");
+        myDB.insertData("InsertionSort", null, "This is done for each preceding number in array until we reach the end of the array");
+
+        myDB.insertData("QuickSort", null, "Select a pivot of your choosing");
+        myDB.insertData("QuickSort", null, "Put all items lower than the pivot to its left and all items larger than the pivot to its right, the pivot is now sorted");
+        myDB.insertData("QuickSort", null, "Repeat the above step for the right side of the pivot as a sub array and do the same for the right side of the pivot");
+
+        myDB.insertData("MergeSort", null, "The algorithm starts by splitting the array half then split those halves in half until there is until each subarray is size one");
+        myDB.insertData("MergeSort", null, "Each pair of halves is sorted then sorts as they merge with each other");
+        myDB.insertData("MergeSort", null, "This method can be looked at as similar to divide and conquer but they sort as they conquer");
+
+        myDB.insertData("HeapSort", null, "Puts array in heap");
+        myDB.insertData("HeapSort", null, "Change heap to max heap (A max heap states that a parent node is always greater than or equal to its child node)");
+        myDB.insertData("HeapSort", null, "Take the root and swap it with the last node in the heap");
+        myDB.insertData("HeapSort", null, "Remove or ignore that node as part of the heap because it has just been sorted");
+        myDB.insertData("HeapSort", null, "Since the array is already in a heap, repeat the rest of the steps to sorted the rest of the nodes in the heap or the rest of the elements in the array");
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener =
