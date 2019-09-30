ee5f8a4636272d7555be5da11f7a8c137d4168cd
ddunig2 <36016544+ddunig2@users.noreply.github.com>
2019-09-29 23:22:27 -0400

    Added content to do while loop.

--- app/src/main/java/com/example/lugdu/datastructuresandalgorithms/algo/iteration/DoWhileLoopFragment.java
+++ app/src/main/java/com/example/lugdu/datastructuresandalgorithms/algo/iteration/DoWhileLoopFragment.java
@@ -1,5 +1,6 @@
 package com.example.lugdu.datastructuresandalgorithms.algo.iteration;
 
+import android.animation.ObjectAnimator;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
@@ -8,15 +9,20 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.example.lugdu.datastructuresandalgorithms.CircleText;
+import com.example.lugdu.datastructuresandalgorithms.MainActivity;
 import com.example.lugdu.datastructuresandalgorithms.PagerAdapter;
 import com.example.lugdu.datastructuresandalgorithms.R;
 import com.example.lugdu.datastructuresandalgorithms.Steps2Fragment;
@@ -32,6 +38,20 @@ public class DoWhileLoopFragment extends Fragment {
     boolean hasStopped = false;
     ViewPager viewPager;
     boolean isRunning;
+    Thread animationThread;
+    int circleSize = 150;
+    int leftMargin = 0;
+    int topMargin = 0;
+    int bottomMargin = 0;
+    int rightMargin = 0;
+    boolean condition1 = true;
+    boolean condition2 = true;
+    int w;
+    SwitchCompat switchStatus1;
+    SwitchCompat switchStatus2;
+
+    RelativeLayout insertionPoint;
+    CircleText[] tArr = new CircleText[2];
     public static int color = Color.parseColor("#19B41C");
     @Nullable
     @Override
@@ -46,10 +66,94 @@ public class DoWhileLoopFragment extends Fragment {
         Animation animation = AnimationUtils.loadAnimation(getContext(), android.R.anim.slide_in_left);
         animation.setDuration(1000);
         def.setAnimation(animation);
+        w = MainActivity.width;
+        insertionPoint = view.findViewById(R.id.animationView);
+        initCircles();
+        switchStatus1 = view.findViewById(R.id.swStatusCustom1);
+        switchStatus2 = view.findViewById(R.id.swStatusCustom2);
+        TextView explaination = view.findViewById(R.id.explanationText);
 
+        switchStatus1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked && switchStatus2.isChecked()){
+                    animateCircles();
+                }
+            }
+        });
+        switchStatus2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked && switchStatus1.isChecked()){
+                    animateCircles();
+                }
+            }
+        });
         setUpViewPager();
+        explaination.setText("do{ circleAnimation() }while(condition1 && condition2);");
+        animateCircles();
         return view;
     }
+
+    public void animateCircles(){
+        animationThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                do{
+                    if(getActivity()!= null && !hasStopped){
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                ObjectAnimator animator = ObjectAnimator.ofFloat(tArr[0],"translationX", tArr[1].getX());
+                                animator.setDuration(1000);
+
+                                ObjectAnimator animator1 = ObjectAnimator.ofFloat(tArr[1],"translationX", tArr[0].getX());
+                                animator1.setDuration(1000);
+
+                                animator.start();
+                                animator1.start();
+                                System.out.println("runing");
+                                //animate
+                            }
+                        });
+                    }
+                    pause(Thread.currentThread(), 1000);
+
+                    if(getActivity()!= null && !hasStopped){
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                ObjectAnimator animator = ObjectAnimator.ofFloat(tArr[0],"translationX", tArr[1].getX());
+                                animator.setDuration(1000);
+
+                                ObjectAnimator animator1 = ObjectAnimator.ofFloat(tArr[1],"translationX", tArr[0].getX());
+                                animator1.setDuration(1000);
+
+                                animator.start();
+                                animator1.start();
+                                System.out.println("runing");
+                                //animate
+                            }
+                        });
+                    }
+
+                    pause(Thread.currentThread(), 1000);
+                }while (switchStatus1.isChecked() && switchStatus2.isChecked());
+            }
+        });
+        animationThread.start();
+    }
+
+    public void pause(Thread thread, int time){
+        synchronized (thread){
+            try {
+                Thread.currentThread().wait(time);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public void setUpViewPager(){
         LinearLayout dotsView = view.findViewById(R.id.sliderDots);
         HashMap<Integer, Fragment> steps = getViewFragments();
@@ -90,6 +194,22 @@ public class DoWhileLoopFragment extends Fragment {
 
             }
         });
+    }
+
+    public void initCircles(){
+        tArr[0] = new CircleText(getContext());
+        tArr[1] = new CircleText(getContext());
+        tArr[1].select(true);
+        tArr[0].setX(20);
+        tArr[1].setX(w - (circleSize + 20));
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(circleSize,circleSize);
+        layoutParams.setMargins(leftMargin,topMargin,rightMargin,bottomMargin);
+
+        RelativeLayout.LayoutParams layoutParams1 = new RelativeLayout.LayoutParams(circleSize,circleSize);
+        layoutParams.setMargins(leftMargin,topMargin,rightMargin,bottomMargin);
+
+        insertionPoint.addView(tArr[0], layoutParams);
+        insertionPoint.addView(tArr[1], layoutParams1);
     }
 
     public HashMap<Integer, Fragment> getViewFragments(){
